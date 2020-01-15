4448d87fa9d537b20ef3eeccd493b52263520b66
mitsu733 <tubbbbbu@yahoo.co.jp>
2020-01-15 19:32:47 +0900

    [Finishi]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '446a8c6df3009af63147dae56444a08ee63b92d4a13a80aa30fcb78d136dafaeaf3adc7c0e4b8abaea135895f119140a91d81841fb3b5aa09a79f5324e1406ce'
