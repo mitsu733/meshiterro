2edf4abc3a61c244effee1e4ab7eadd21a075184
mitsu733 <tubbbbbu@yahoo.co.jp>
2020-01-18 16:21:03 +0900

    [Finish]5-7

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ resources :post_images, only: [:new, :create, :index, :show, :destroy] do
 	resource :post_comments, only: [:create, :destroy]
 	resource :favorites, only: [:create, :destroy]
 end
-resources :users, only: [:show]
+resources :users, only: [:show, :edit, :update]
 end
